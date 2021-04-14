79017718e5ae58bfb6464be01f4c53604680394a
Catcheryp <2281718195@qq.com>
2021-04-14 08:26:59 +0800

    添加职位详情页面

--- django_demo/recruitment/jobs/views.py
+++ django_demo/recruitment/jobs/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from jobs.models import Job
 from jobs.models import Cities, JobTypes
@@ -17,3 +18,12 @@ def joblist(request):
         job.job_type = JobTypes[job.job_type][1]
 
     return HttpResponse(template.render(context))
+
+def detail(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        job.city_name = Cities[job.job_city][1]
+    except Job.DoesNotExist:
+        raise Http404("Job does not exist")
+    
+    return render(request, 'job.html', {'job':job})
